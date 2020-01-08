9696e35819c1cb9d8a8234c9861d8897d7963ab0
91074 <910749592@qq.com>
2020-01-09 00:41:09 +0800

    jsd customer font

--- src/request/img/login.js
+++ src/request/img/login.js
@@ -1,7 +1,4 @@
 import bg1 from '@/assets/img/login/1.jpg'
 import bg2 from '@/assets/img/login/2.jpg'
-import bg3 from '@/assets/img/login/3.jpg'
-import bg4 from '@/assets/img/login/4.jpg'
-import bg5 from '@/assets/img/login/5.jpg'
 
-export default [bg1, bg2, bg3, bg4, bg5]
+export default [bg1, bg2]
