c4efa8ef6b33ca5ebda054845c2e30309d802eae
91074 <910749592@qq.com>
2020-01-09 00:27:06 +0800

    jsd customer font

--- src/request/img/register.js
+++ src/request/img/register.js
@@ -1,7 +1,4 @@
 import bg1 from '@/assets/img/register/1.jpg'
 import bg2 from '@/assets/img/register/2.jpg'
-import bg3 from '@/assets/img/register/3.jpg'
-import bg4 from '@/assets/img/register/4.jpg'
-import bg5 from '@/assets/img/register/5.jpg'
 
-export default [bg1, bg2, bg3, bg4, bg5]
+export default [bg1, bg2]
